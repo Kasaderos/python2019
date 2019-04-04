296cae2461dfb4f46d9b29ab97886048521f4e77
kasaderos <hakaze0000@gmail.com>
2019-04-04 10:33:50 +0300

    My first button QUIT

--- example.py
+++ example.py
@@ -1,6 +1,9 @@
 from tkinter import *
 
+def back():
+    root.quit()
+
 root = Tk()
-btn1 = Button(root, text='quit', width=25, height=5, bg='black', fg='red')
-btn1.pack()
+btn1 = Button(root, text='quit', width=25, height=5, bg='black', fg='red', command=back)
+btn1.grid()
 root.mainloop()
