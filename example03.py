a60b10026c4a94384cbc2704b240123d6fddeb2d
kasaderos <hakaze0000@gmail.com>
2019-04-18 02:53:51 +0300

    example03 finished, added window resizable features

--- example03.py
+++ example03.py
@@ -12,11 +12,12 @@ def add_btn():
     stylish = ttk.Style()
     stylish.configure(color + "." + "TButton", background=color)
     btn = ttk.Button(frame, text="Color", style=color + "." + "TButton")
-    lbl = ttk.Label(frame, text="Color", background=color)
+    lbl = ttk.Label(frame, text="Color", background=color, anchor="center")
     column, row = frame.size()
-    btn.grid(row=row + 1, column=0)
-    lbl.grid(row=row + 1, column=1)
-
+    btn.grid(row=row + 1, column=0, sticky="news")
+    lbl.grid(row=row + 1, column=1, sticky="news")
+        
+    frame.rowconfigure(row+1, weight=1)
 
 root = Tk()
 root.title("example03")
@@ -29,4 +30,15 @@ btn_exit = ttk.Button(frame, text="Exit", command=root.quit)
 
 btn_add.grid(row=0, column=0, sticky="news")
 btn_exit.grid(row=0, column=1, sticky="news")
+
+root.columnconfigure(0, weight=1)
+root.rowconfigure(0, weight=1)
+frame.columnconfigure(0, weight=1)
+frame.columnconfigure(1, weight=1)
+frame.rowconfigure(0, weight=1)
+frame.rowconfigure(0, weight=1)
+#btn_add.columnconfigure(0, weight=1)
+#btn_add.rowconfigure(0, weight=1)
+#btn_exit.columnconfigure(0, weight=1)
+#btn_exit.rowconfigure(0, weight=1)
 root.mainloop()
