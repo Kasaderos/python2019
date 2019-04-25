3540e161c7a0ea523eb2d17269c5293371f5a869
kasaderos <hakaze0000@gmail.com>
2019-04-25 21:26:49 +0300

    config took out to separate function

--- 06_devnull.py
+++ 06_devnull.py
@@ -6,21 +6,32 @@
 from tkinter import *
 from tkinter import colorchooser
 
+
 class App(Frame):
     '''Base framed application class'''
+
     def __init__(self, master=None, Title="Application"):
         Frame.__init__(self, master)
         self.master.rowconfigure(0, weight=1)
         self.master.columnconfigure(0, weight=1)
         self.master.title(Title)
-        self.grid(sticky=N+E+S+W)
+        self.grid(sticky=N + E + S + W)
         self.create()
-        
+        self.config()
 
     def create(self):
         '''Create all the widgets'''
         self.bQuit = Button(self, text='Quit', command=self.quit)
         self.bQuit.grid(row=0, column=1)
+
+    def config(self):
+        self.rowconfigure(2, weight=1)
+        self.columnconfigure(0, weight=1)
+        self.columnconfigure(1, weight=1)
+        self.columnconfigure(2, weight=1)
+        self.rowconfigure(0, weight=1)
+        self.rowconfigure(1, weight=1)
+
 '''
     def adjust(self):
         
@@ -30,23 +41,22 @@ class App(Frame):
         for i in range(self.size()[1]):
             self.rowconfigure(i, weight=12)
 '''
+
+
 class Paint(Canvas):
-    
+
     def mousedown(self, event):
-    
         self.x0, self.y0 = event.x, event.y
         self.cursor = None
 
     def mousemove(self, event):
-    
         if self.cursor:
             self.delete(self.cursor)
         self.cursor = self.create_line((self.x0, self.y0, event.x, event.y), fill=self.foreground.get())
 
     def mouseup(self, event):
-    
         self.cursor = None
-        #print(self.find_all())
+        # print(self.find_all())
 
     def __init__(self, master=None, *ap, foreground="black", **an):
         self.foreground = StringVar()
@@ -56,7 +66,9 @@ class Paint(Canvas):
         self.bind("<B1-Motion>", self.mousemove)
         self.bind("<ButtonRelease-1>", self.mouseup)
 
+
 class MyApp(App):
+
     def askcolor(self):
         self.Canvas.foreground.set(colorchooser.askcolor()[1])
         self.ShowColor['bg'] = self.Canvas.foreground.get()
@@ -64,24 +76,20 @@ class MyApp(App):
     def create(self):
         self.Canvas = Paint(self, foreground="black")
         self.Canvas.foreground.set("#000000")
-        self.Canvas.grid(row=0, column=0, rowspan=3, sticky=N+E+S+W)
+        self.Canvas.grid(row=0, column=0, rowspan=3, sticky=N + E + S + W)
+        self.Canvas2 = Paint(self, foreground="black")
+        self.Canvas2.foreground.set("#000000")
+        self.Canvas2.grid(row=0, column=2, rowspan=3, sticky=N + E + S + W)
         self.AskColor = Button(self, text="Color", command=self.askcolor)
-        self.AskColor.grid(row=0, column=1, sticky=W+N+S+E)
-        self.rowconfigure(0, weight=1)
+        self.AskColor.grid(row=0, column=1, sticky=W + N + S + E)
         self.ShowColor = Label(self, textvariable=self.Canvas.foreground)
-        self.ShowColor.grid(row=1, column=1, sticky=W+N+S+E)
-        self.rowconfigure(1, weight=1)
+        self.ShowColor.grid(row=1, column=1, sticky=W + N + S + E)
+        self.ShowColor['bg'] = self.Canvas.foreground.get()
         self.Quit = Button(self, text="Quit", command=self.quit)
-        self.Quit.grid(row=2, column=1, sticky=W+N+S+E)
-        self.rowconfigure(2, weight=1)
-        self.columnconfigure(0, weight=1)
-        self.columnconfigure(1, weight=1)
-		
-        
-	
-	
+        self.Quit.grid(row=2, column=1, sticky=W + N + S + E)
+
+
 app = MyApp(Title="Canvas Example")
 app.mainloop()
-#for item in app.Canvas.find_all():
- #   print(*app.Canvas.coords(item), app.Canvas.itemcget(item, "fill"))
-
+# for item in app.Canvas.find_all():
+#   print(*app.Canvas.coords(item), app.Canvas.itemcget(item, "fill"))
